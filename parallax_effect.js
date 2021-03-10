1e0c1a8caea2a40e48fe273bf307af33576aeb5e
Oliver Řezníček <u19.423@pslib.cloud>
2021-03-10 20:32:14 +0100

    parallax only changing when the user sees it

--- parallax_effect.js
+++ parallax_effect.js
@@ -2,10 +2,14 @@ const layer1 = document.getElementsByClassName("layer-1")[0];
 const layer2 = document.getElementsByClassName("layer-2")[0];
 const layer3 = document.getElementsByClassName("layer-3")[0];
 const layer4 = document.getElementsByClassName("layer-4")[0];
+const container = document.getElementsByClassName("parallax-container")[0];
 
 window.onscroll = (e) => {
-    layer4.style.transform = "translateY(-"+window.scrollY+"px)";
-    layer3.style.transform = "translateY(-"+window.scrollY+"px) translateX(-"+window.scrollY/2+"px)";
-    layer2.style.transform = "translateY(-"+window.scrollY/2+"px)";
-    layer1.style.transform = "translateY(-"+window.scrollY/2+"px)";
+    if (window.scrollY <= container.offsetHeight){
+        console.log(window.scrollY)
+        layer4.style.transform = "translateY(-"+window.scrollY+"px)";
+        layer3.style.transform = "translateY(-"+window.scrollY+"px) translateX(-"+window.scrollY/2+"px)";
+        layer2.style.transform = "translateY(-"+window.scrollY/2+"px)";
+        layer1.style.transform = "translateY(-"+window.scrollY/2+"px)";
+    }
 }
